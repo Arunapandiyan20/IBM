05a5dab0dbeb011ac120292c6e5c09fb9479d48b
Aruna-07 <112760546+Aruna-07@users.noreply.github.com>
2022-09-23 13:58:36 +0530

    Add files via upload

---
+++ Assignment/Team Member-1/Assignment-1/coding 1 output.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========== RESTART: C:\Users\DEVASHREE\OneDrive\Desktop\coding 1.py ===========
+[11, 21, 31, 41, 51]
+[11, 21, 31, 41, 100, 51]
+[21, 31, 41, 100, 51]
+[21, 31, 41, 100, 51, -1]
+[-1, 21, 31, 41, 51, 100]
+[-1, 21, 31, 41, 51]
+[51, 41, 31, 21, -1]
+WelcomeYou
+lla uoy emocleW
+ecm
---
+++ Assignment/Team Member-1/Assignment-1/coding 1.py
@@ -0,0 +1,54 @@
+
+
+list=[11,21,31,41,51]
+print(list)
+
+#insertion
+list.insert(4,100)
+print(list)
+
+#deletion of first occurence of integer
+del list[0]
+print(list)
+
+#insertion at end of list
+list.append(-1)
+print(list)
+
+#sorting
+list.sort()
+print(list)
+
+#popping last element
+list.pop()
+print(list)
+
+#reverse the list
+list.reverse()
+print(list)
+
+#STRING OPERATIONS:
+
+#concatenate string
+str1=("Welcome")
+str2=("You")
+print(str1+str2)
+
+#reverse of String
+str3="Welcome you all"
+print(str3[::-1])
+
+#slice
+print(str3[slice(1,6,2)])
+
+
+
+
+
+
+
+
+
+
+
+
