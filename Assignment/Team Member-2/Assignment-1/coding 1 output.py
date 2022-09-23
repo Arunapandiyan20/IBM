0c589ff68e83b7e41b9d869a88cd6625d7fc8d25
DevashreeRanjitha <113416593+DevashreeRanjitha@users.noreply.github.com>
2022-09-23 14:26:10 +0530

    Add files via upload

---
+++ Assignment/Team Member-2/Assignment-1/coding 1 output.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========== RESTART: C:\Users\DEVASHREE\OneDrive\Desktop\coding 1.py ===========
+[101, 201, 301, 401, 501]
+[101, 201, 301, 401, 501, 100]
+[201, 301, 401, 501, 100]
+[201, 301, 401, 501, 100, -1]
+[-1, 100, 201, 301, 401, 501]
+[-1, 100, 201, 301, 401]
+[401, 301, 201, 100, -1]
+NewYork
+yaD ecin a evaH
+aea
---
+++ Assignment/Team Member-2/Assignment-1/coding 1.py
@@ -0,0 +1,54 @@
+
+
+list=[101,201,301,401,501]
+print(list)
+
+#insertion
+list.insert(15,100)
+print(list)
+
+#deletion of first occurence of integer
+del list[0]
+print(list)
+
+#insertion at end of list
+list.append(-1)
+print(list)
+
+#sorting
+list.sort()
+print(list)
+
+#popping last element
+list.pop()
+print(list)
+
+#reverse the list
+list.reverse()
+print(list)
+
+#STRING OPERATIONS:
+
+#concatenate string
+str1=("New")
+str2=("York")
+print(str1+str2)
+
+#reverse of String
+str3="Have a nice Day"
+print(str3[::-1])
+
+#slice
+print(str3[slice(1,6,2)])
+
+
+
+
+
+
+
+
+
+
+
+
