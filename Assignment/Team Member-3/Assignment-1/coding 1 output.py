9f5704ee064d363d57b56b2ddbd7b77580e9e18e
KavyaRaja30 <113846831+KavyaRaja30@users.noreply.github.com>
2022-09-23 14:39:47 +0530

    Add files via upload

---
+++ Assignment/Team Member-3/Assignment-1/coding 1 output.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========== RESTART: C:\Users\DEVASHREE\OneDrive\Desktop\coding 1.py ===========
+[1, 2, 3, 4, 5]
+[1, 100, 2, 3, 4, 5]
+[100, 2, 3, 4, 5]
+[100, 2, 3, 4, 5, -1]
+[-1, 2, 3, 4, 5, 100]
+[-1, 2, 3, 4, 5]
+[5, 4, 3, 2, -1]
+PassPort
+egdE tfosorciM
+irs
---
+++ Assignment/Team Member-3/Assignment-1/coding 1.py
@@ -0,0 +1,54 @@
+
+
+list=[1,2,3,4,5]
+print(list)
+
+#insertion
+list.insert(1,100)
+print(list)
+
+#deletion of first occurence of integer
+del list[0]
+print(list)
+
+#insertion at end of list
+list.append(-1)
+print(list)
+
+#sorting
+list.sort()
+print(list)
+
+#popping last element
+list.pop()
+print(list)
+
+#reverse the list
+list.reverse()
+print(list)
+
+#STRING OPERATIONS:
+
+#concatenate string
+str1=("Pass")
+str2=("Port")
+print(str1+str2)
+
+#reverse of String
+str3="Microsoft Edge"
+print(str3[::-1])
+
+#slice
+print(str3[slice(1,6,2)])
+
+
+
+
+
+
+
+
+
+
+
+
