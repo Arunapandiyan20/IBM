3588fb36558c54b88dd87b8a51bed5a282104b0b
Arunapandiyan20 <113448605+Arunapandiyan20@users.noreply.github.com>
2022-09-23 13:40:30 +0530

    Add files via upload

---
+++ Assignment/Team Lead/Assignment-1/coding 1 output.py
@@ -0,0 +1,14 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========== RESTART: C:\Users\DEVASHREE\OneDrive\Desktop\coding 1.py ===========
+[10, 20, 30, 40, 50]
+[10, 20, 30, 40, 50, 100]
+[20, 30, 40, 50, 100]
+[20, 30, 40, 50, 100, -15]
+[-15, 20, 30, 40, 50, 100]
+[-15, 20, 30, 40, 50]
+[50, 40, 30, 20, -15]
+HelloIndia
+erolgnaB
+ago
---
+++ Assignment/Team Lead/Assignment-1/coding 1.py
@@ -0,0 +1,54 @@
+
+
+list=[10,20,30,40,50]
+print(list)
+
+#insertion
+list.insert(5,100)
+print(list)
+
+#deletion of first occurence of integer
+del list[0]
+print(list)
+
+#insertion at end of list
+list.append(-15)
+print(list)
+
+#sorting
+list.sort()
+print(list)
+
+#popping last element
+list.pop()
+print(list)
+
+#reverse the list
+list.reverse()
+print(list)
+
+#STRING OPERATIONS:
+
+#concatenate string
+str1=("Hello")
+str2=("India")
+print(str1+str2)
+
+#reverse of String
+str3="Banglore"
+print(str3[::-1])
+
+#slice
+print(str3[slice(1,6,2)])
+
+
+
+
+
+
+
+
+
+
+
+
